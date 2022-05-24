d1f8ec160b6015b1b9abd6b01c196d6df8569f7d
ZhengJieGan <ganzj45@gmail.com>
2022-05-24 19:43:21 +0800

    finished part 1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  
   def index
     @articles = Article.all
   end
@@ -19,6 +22,27 @@ class ArticlesController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get "/articles", to: "articles#index"
   get "/articles/new", to: "articles#new"
   get "/articles/:id", to: "articles#show"
+  get "/articles/:id/edit", to: "articles#edit"
+  
+  
   
 
   resources :articles
